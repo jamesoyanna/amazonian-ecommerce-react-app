752b9ef7d4ee31dbc882e8a4699368c73c314273
james <engrjayt200@gmail.com>
2021-04-21 16:52:41 +0100

    Install multerS3 to handle file upload to aws

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -1,32 +1,42 @@
-import { config } from "dotenv/types";
 import express from "express";
 import multer from "multer";
-import mluterS3 from  "multer-s3";
-import aws from "../config";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+import config from "../config";
 
-const storage = mluter.diskStorage({
-    destination(req, file, cb){
-        cb(null, 'uploads');
-    },
-    filename(req, file, cb){
-        cb(null, `${Date.now()}.jpg`);
-    },
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename(req, file, cb) {
+    cb(null, `${Date.now()}.jpg`);
+  },
 });
 
-const uplaod = multer({storage});
+const upload = multer({ storage });
+
 const router = express.Router();
 
-router.post("/", uplaod.single('image'), (req, res) =>{
-    res.send(`/${req.file.path}`);
-})
-
-aws.config.update({
-    accessKeyId: config.accessKeyId,
-    secretAccessKey: config.secretAccessKey,
+router.post("/", upload.single("image"), (req, res) => {
+  res.send(`/${req.file.path}`);
 });
 
+aws.config.update({
+  accessKeyId: config.accessKeyId,
+  secretAccessKey: config.secretAccessKey,
+});
 const s3 = new aws.S3();
-const storageS3 = mluterS3({
-    s3,
-    bucket: 'amazona-bucket'
-})
+const storageS3 = multerS3({
+  s3,
+  bucket: "amazona-bucket",
+  acl: "public-read",
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  key(req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const uploadS3 = multer({ storage: storageS3 });
+router.post("/s3", uploadS3.single("image"), (req, res) => {
+  res.send(req.file.location);
+});
+export default router;
